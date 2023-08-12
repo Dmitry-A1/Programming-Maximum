c4b72d8ea21a660baab1795c10f4d6601974afa6
Dima <anurov.dm@gmail.com>
2023-08-12 07:48:10 +0300

    Слой Tamplates и Static

--- advirtisments/app_adviritisments/views.py
+++ advirtisments/app_adviritisments/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Good')
+    return HttpResponse(request, 'index.html')
+
+def top_sellers(request):
+    return render (request, 'top_sellers.html')
